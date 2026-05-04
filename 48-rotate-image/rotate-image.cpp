476656b1788a6973e0b367ddd6e555c351d05a30
Ceastin <129533667+Ceastin@users.noreply.github.com>
2026-05-04 09:10:04 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (99.74%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -5,15 +5,15 @@ public:
         {
             for(int j=0;j<i;j++)
             {
-                int temo=matrix[i][j];
+                int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temo;
+                matrix[j][i]=temp;
             }
         }
         for(int i=0;i<matrix.size();i++)
         {
             reverse(matrix[i].begin(),matrix[i].end());
         }
-        return ;
+        
     }
 };
